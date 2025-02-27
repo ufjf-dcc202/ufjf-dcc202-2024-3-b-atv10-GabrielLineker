f6db37523cbd69dd5a582f5d024ea25fb1d25f26
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2025-02-27 20:03:10 -0300

    atv10

---
+++ main.js
@@ -0,0 +1,11 @@
+import { deJoaoParaMaria, getJoao, getMaria } from "./joaoEMaria";
+
+console.log("módulo main");
+
+console.log("João tem ", getJoao(), " maçãs!");
+console.log("Maria tem ", getMaria(), " maçãs!");
+
+deJoaoParaMaria();
+
+console.log("João tem ", getJoao(), " maçãs!");
+console.log("Maria tem ", getMaria(), " maçãs!");
